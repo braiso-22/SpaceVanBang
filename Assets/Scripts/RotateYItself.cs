397a84e1d03d990d367805b7068abb26a50b4572
Brais <braisfv22@gmail.com>
2022-02-13 17:03:40 +0100

    Menu de inicio empezado

---
+++ Assets/Scripts/RotateYItself.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateYItself : MonoBehaviour
+{
+    public float vel;
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up * Time.deltaTime * vel);
+    }
+}
