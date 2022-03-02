a048488bbecbfade79201a41a95e83e38e7b5f75
Brais <braisfv22@gmail.com>
2022-03-02 23:02:08 +0100

    Se generan asteroides pequeños al rededor

--- Assets/Scripts/Levels/ChargeLevel1.cs
+++ Assets/Scripts/Levels/ChargeLevel1.cs
@@ -19,14 +19,38 @@ public class ChargeLevel1 : MonoBehaviour
             position = new Vector3(generateRandomNumber(-45f, 45f), generateRandomNumber(-35f, 35f), position.z);
             gravityObject.transform.position = position;
         }
-        
+
     }
 
-    void Start(){
-        for (int i = -200; i < 200; i++)
+    void Start()
+    {
+        generateMiniAsteroides(0, -100f, 100f,200,-200f);
+
+        generateMiniAsteroides(1.5f, 200f, 400f,500f,400f);
+        generateMiniAsteroides(0.5f, 0, 200f,400f,200f);
+        
+        generateMiniAsteroides(-1.5f, 0f, 200f,-400f,-500f);
+        generateMiniAsteroides(-0.5f, -100f, 100f, -200f, -400f);
+
+    }
+
+    float generateRandomNumber(float min, float max)
+    {
+        return Random.Range(min, max);
+    }
+
+    // metodo para generar los mini asteroides
+    void generateMiniAsteroides(float diagonal,float xInicio, float xFinal, float zInicio, float zFinal)
+    {
+        float counter = 0f;
+        for (float i = zInicio; i > zFinal; i--)
         {
-            Vector3 position = new Vector3(generateRandomNumber(-100f,100f), generateRandomNumber(-100f,100f), i);
-            GameObject instantiatedObject = Instantiate(miniAsteroide, position, Quaternion.identity);
+
+            Vector3 position = new Vector3(generateRandomNumber(xInicio - counter, xFinal - counter), generateRandomNumber(-120f, 100f), i);
+            counter += diagonal;
+            //  generate random rotation 
+            Quaternion rotation = Quaternion.Euler(generateRandomNumber(0, 360), generateRandomNumber(0, 360), generateRandomNumber(0, 360));
+            GameObject instantiatedObject = Instantiate(miniAsteroide, position, rotation);
 
             if (instantiatedObject.GetComponent<InSamePosition>().getSamePosition())
             {
@@ -35,10 +59,7 @@ public class ChargeLevel1 : MonoBehaviour
                 i--;
             }
         }
-    }
 
-    float generateRandomNumber(float min, float max)
-    {
-        return Random.Range(min, max);
+
     }
 }
